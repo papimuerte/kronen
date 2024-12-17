fa5e8178dd95afac4567d3fc8539e67455b5ed11
bilalboukayoua <bilal.boukayoua@stud.fra-uas.de>
2024-12-17 15:40:58 +0100

    initalisierung File Reader/Write

--- scm/src/main/java/com/scm/scm/util/FileStorage.java
+++ scm/src/main/java/com/scm/scm/util/FileStorage.java
@@ -1,5 +1,19 @@
 package com.scm.scm.util;
 
-public class FileStorage {
-    
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileStorage<T> {
+
+    private final File file;
+    private final ObjectMapper objectMapper;
+
+    public FileStorage(String filePath) {
+        this.file = new File(filePath);
+        this.objectMapper = new ObjectMapper();
+}
 }
