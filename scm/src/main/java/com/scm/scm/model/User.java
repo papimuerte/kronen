137951a09ec15ff0523902e6d9b2ab53cf5bd8d5
YassinMelha <yassin.melha@web.de>
2024-12-20 19:34:08 +0100

    user model erstellen für die authentification

--- scm/src/main/java/com/scm/scm/model/User.java
+++ scm/src/main/java/com/scm/scm/model/User.java
@@ -1,8 +1,53 @@
 package com.scm.scm.model;
 
-public class Customer {
-    
-    private Long id;
-    private String name;
-    private String email;
-}                              
+public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    // Default constructor
+    public User() {
+    }
+
+    // Parameterized constructor
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // toString method (optional for debugging)
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
