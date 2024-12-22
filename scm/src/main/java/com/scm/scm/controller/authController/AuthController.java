7e0205969fdb700e8b1c84435864a6e7ba4f42ab
YassinMelha <yassin.melha@web.de>
2024-12-22 18:45:21 +0100

    Füge Registrierungsfunktion hinzu

--- scm/src/main/java/com/scm/scm/controller/authController/AuthController.java
+++ scm/src/main/java/com/scm/scm/controller/authController/AuthController.java
@@ -1,9 +1,15 @@
 package com.scm.scm.controller.authController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.scm.scm.model.User;
 import com.scm.scm.util.UserDataUtil;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -13,5 +19,20 @@ public class AuthController {
 
     public AuthController(UserDataUtil userDataUtil) {
         this.userDataUtil = userDataUtil;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        try {
+            List<User> users = userDataUtil.loadUsers();
+            if (users.stream().anyMatch(u -> u.getUsername().equals(user.getUsername()))) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Benutzername existiert bereits.");
+            }
+            users.add(user);
+            userDataUtil.saveUsers(users);
+            return ResponseEntity.ok("Benutzer erfolgreich registriert.");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Fehler beim Zugriff auf Benutzerdaten.");
+        }
     }
 }
