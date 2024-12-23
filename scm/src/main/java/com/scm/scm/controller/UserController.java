7b72563da6f7b984941460d411fb7c3625cb86d3
hzmdwd2003 <hazemdawood2023@gmail.com>
2024-12-23 22:34:47 +0100

    feat: Implementierung von GET-Endpunkten im UserController
    - GET /users: Gibt alle Benutzer zurück
    - GET /users/{email}: Gibt einen Benutzer anhand der E-Mail zurück
    - Fehlerbehandlung bei IOExceptions integriert

--- scm/src/main/java/com/scm/scm/controller/UserController.java
+++ scm/src/main/java/com/scm/scm/controller/UserController.java
@@ -1,19 +1,46 @@
 package com.scm.scm.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.scm.scm.model.User;
+import com.scm.scm.util.UserDataUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
 
-
-@Controller
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    @RequestMapping(value = "scm", method=RequestMethod.GET)
-    @ResponseBody
-    public String getHelloWorld() 
-    {
-        return "<h1>Erste Einrichtung des Projektes</h1>";
+
+    private final UserDataUtil userDataUtil;
+
+    public UserController(UserDataUtil userDataUtil) {
+        this.userDataUtil = userDataUtil;
     }
-    
+
+    // GET /users: Abrufen aller Benutzer
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        try {
+            List<User> users = userDataUtil.loadUsers();
+            return ResponseEntity.ok(users);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    // GET /users/{email}: Anzeigen eines spezifischen Benutzers anhand der E-Mail
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        try {
+            List<User> users = userDataUtil.loadUsers();
+            return users.stream()
+                    .filter(user -> user.getEmail().equalsIgnoreCase(email))
+                    .findFirst()
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
