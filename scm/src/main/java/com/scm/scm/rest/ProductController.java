6b9427aeacb23be62d72564c36d48475fbeb328e
bilalboukayoua <bilal.boukayoua@stud.fra-uas.de>
2024-12-23 22:03:48 +0100

    Alle Produkte werden durch die Methode abgerufen

--- scm/src/main/java/com/scm/scm/rest/ProductController.java
+++ scm/src/main/java/com/scm/scm/rest/ProductController.java
@@ -1,6 +1,12 @@
 package com.scm.scm.rest;
 
+import com.scm.scm.model.Product;
 import com.scm.scm.util.ProductDataUtil;
+import java.io.IOException;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,4 +18,17 @@ public class ProductController {
     public ProductController(ProductDataUtil productDataUtil) {
         this.productDataUtil = productDataUtil;
     }
+    
+    
+        // Alle Produkte abrufen (READ)
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        try {
+            List<Product> products = productDataUtil.loadProducts();
+            return ResponseEntity.ok(products);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
 }
