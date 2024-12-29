cd3621012fe366d36b6c5b75db1cb731b5497f7e
aymiu <altahir.ayman.ffm@gmail.com>
2024-12-29 16:36:31 +0100

    GraphQL-Konfiguration hinzugefügt: RuntimeWiringConfigurer implementiert für Abfragen und Mutationen zu Bestellungen

---
+++ scm/src/main/java/com/scm/scm/config/GraphQLConfig.java
@@ -0,0 +1,34 @@
+package com.scm.scm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scm.scm.graphql.OrderInput;
+import com.scm.scm.graphql.OrderResolver;
+
+@Configuration
+public class GraphQLConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer(OrderResolver orderResolver) {
+        return wiringBuilder -> wiringBuilder
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("orders", env -> {
+                            String customerUsername = env.getArgument("customerUsername");
+                            return orderResolver.getOrders(customerUsername);
+                        })
+                        .dataFetcher("order", env -> {
+                            String id = env.getArgument("id");
+                            return orderResolver.getOrder(id);
+                        }))
+                .type("Mutation", typeWiring -> typeWiring
+                    .dataFetcher("createOrder", env -> {
+                        // Convert the raw input to OrderInput
+                        ObjectMapper mapper = new ObjectMapper();
+                        OrderInput input = mapper.convertValue(env.getArgument("input"), OrderInput.class);
+                        return orderResolver.createOrder(input);
+                    }));
+    }
+}
