4d6ba3c208418777eccf93ccadcacf64ee746e53
aymiu <altahir.ayman.ffm@gmail.com>
2024-12-29 16:19:39 +0100

    ProductInput-Klasse hinzugefügt, um GraphQL-Eingaben für Produkt-ID und Menge zu repräsentieren

---
+++ scm/src/main/java/com/scm/scm/graphql/ProductInput.java
@@ -0,0 +1,23 @@
+package com.scm.scm.graphql;
+
+public class ProductInput {
+    private String productId;
+    private int quantity;
+
+    // Getters and setters
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
