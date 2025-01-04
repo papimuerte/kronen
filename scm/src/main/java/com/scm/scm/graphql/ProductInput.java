003ac99685e62d02929ad77cc6f8230aede47ccd
GorangMad <ggorangmadaan@gmail.com>
2025-01-04 16:23:17 +0100

    update ProductInput values

--- scm/src/main/java/com/scm/scm/graphql/ProductInput.java
+++ scm/src/main/java/com/scm/scm/graphql/ProductInput.java
@@ -3,10 +3,20 @@ package com.scm.scm.graphql;
 public class ProductInput {
     private String productId;
     private int quantity;
+    private double unitPrice;
+    private String name;
 
     // Getters
     public String getProductId() {
         return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
     }
 
     public int getQuantity() {
@@ -16,6 +26,14 @@ public class ProductInput {
     // Setters
     public void setProductId(String productId) {
         this.productId = productId;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setQuantity(int quantity) {
