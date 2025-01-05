aa5b27c94715c3430ddbcc9de874d42daba8869c
GorangMad <ggorangmadaan@gmail.com>
2025-01-05 12:38:40 +0100

    checkAvailability logik hinzugefügt in InventoryServiceImpl.java

--- scm/src/main/java/com/scm/scm/grpc/InventoryServiceImpl.java
+++ scm/src/main/java/com/scm/scm/grpc/InventoryServiceImpl.java
@@ -24,4 +24,35 @@ public class InventoryServiceImpl extends InventoryServiceGrpc.InventoryServiceI
         this.inventoryDataUtil = inventoryDataUtil;
     }
 
+    @Override
+    public void checkAvailability(ProductRequest request, StreamObserver<ProductResponse> responseObserver) {
+        try {
+            Optional<Product> productOptional = inventoryDataUtil.getProductById(request.getProductId());
+
+            if (productOptional.isPresent()) {
+                Product product = productOptional.get();
+                boolean isAvailable = product.getAvailableQuantity() >= request.getQuantity();
+                
+                ProductResponse response = ProductResponse.newBuilder()
+                        .setProductId(product.getProductId())
+                        .setAvailable(isAvailable)
+                        .setAvailableQuantity(product.getAvailableQuantity())
+                        .build();
+
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            } else {
+                responseObserver.onError(Status.NOT_FOUND
+                        .withDescription("Product not found: " + request.getProductId())
+                        .asRuntimeException());
+            }
+        } catch (Exception e) {
+            System.err.println("Error during checkAvailability: " + e.getMessage());
+            e.printStackTrace();
+            responseObserver.onError(Status.UNKNOWN
+                    .withDescription("Unknown error occurred")
+                    .withCause(e)
+                    .asRuntimeException());
+        }
+    }
 }
