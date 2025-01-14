ff6825fa567a0efbf9def3518bc10ea1f6c40350
YassinMelha <yassin.melha@web.de>
2025-01-14 20:58:32 +0100

    AuthPage als Einstiegspunkt hinzugefügt

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AuthPage from './pages/AuthPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthPage></AuthPage>
   </React.StrictMode>
 );
