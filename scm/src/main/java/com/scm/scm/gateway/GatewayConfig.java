96971c322a18ef00b42cc508444bf79f472e1f21
aymiu <altahir.ayman.ffm@gmail.com>
2025-01-14 18:43:46 +0100

    Korrektur der Routenpfade durch Hinzufügen von Wildcards ('/**') für REST, GraphQL und gRPC APIs sowie Anpassung der Login- und Registrierungsrouten

--- scm/src/main/java/com/scm/scm/gateway/GatewayConfig.java
+++ scm/src/main/java/com/scm/scm/gateway/GatewayConfig.java
@@ -12,24 +12,24 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 // Route to REST API
-                .route("rest-api", r -> r.path("/api/products/**")
+                .route("rest-api", r -> r.path("/api/products/")
                         .uri("http://localhost:8081")) // REST service URL
 
                 // Route to GraphQL API
-                .route("graphql-api", r -> r.path("/graphql/**")
+                .route("graphql-api", r -> r.path("/graphql/")
                         .uri("http://localhost:8082")) // GraphQL service URL
 
                 // Route to gRPC services
-                .route("grpc-api", r -> r.path("/grpc/**")
+                .route("grpc-api", r -> r.path("/grpc/")
                         .uri("http://localhost:8083")) // gRPC service URL
 
                  // Route to Login
-                 .route("auth-login", r -> r.path("/auth/login")
+                 .route("auth-login", r -> r.path("/auth/")
                  .uri("http://localhost:8084")) // Login service URL
 
                 // Route to Registration
                  .route("auth-register", r -> r.path("/auth/register")
                  .uri("http://localhost:8085")) // Registration service URL
                 .build();
-    }
+    }
 }
