b4eab4c60fa008ddd5a367a8df1fa6968be8c0f4
YassinMelha <yassin.melha@web.de>
2025-01-25 15:43:28 +0100

    hinzufügen der DetailsPage Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,13 @@ import AuthPage from './pages/AuthPage'; // Import your pages
 import AdminOrdersPage from './pages/AdminOrdersPage';
 import ShopPage from './pages/ShopPage';
 import CartPage from './pages/CartPage';
+import DetailsPage from './pages/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap is loaded
 
 const App = () => {
-  const [cart, setCart] = useState([]); // Move cart state to a higher level
+  const [cart] = useState([]); // Move cart state to a higher level
+
+  const token = localStorage.getItem('authToken'); // Retrieve Bearer token
 
   return (
     <Router>
@@ -16,6 +19,7 @@ const App = () => {
         <Route path="/" element={<AuthPage />} />
         <Route path="/admin-orders" element={<AdminOrdersPage />} />
         <Route path="/cart" element={<CartPage cart={cart} />} />
+        <Route path="/details" element={<DetailsPage token={token} />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="*" element={<div className="text-center mt-5">Page Not Found</div>} />
       </Routes>
