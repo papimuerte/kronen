7919a2a24016c61269ca3969442a43a03df14683
GorangMad <ggorangmadaan@gmail.com>
2025-01-27 15:23:54 +0100

    jwt import verbesserung

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const DetailsPage = ({ token }) => {
   const [formData, setFormData] = useState({
