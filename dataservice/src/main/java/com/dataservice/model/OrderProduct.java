83796c59cb8533747c07ff4b212ca268d4cdf8b3
hzmdwd2003 <hazemdawood2023@gmail.com>
2025-02-09 14:19:44 +0100

    Hinzufügen der Modelle

---
+++ dataservice/src/main/java/com/dataservice/model/OrderProduct.java
@@ -0,0 +1,21 @@
+package com.dataservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OrderProduct {
+    private String productId;
+    private String name;
+    private int quantity;
+    private Double unitPrice;
+
+    // Constructor for order-specific products
+    public OrderProduct(String productId, String name, int quantity, Double unitPrice) {
+        this.productId = productId;
+        this.name = name;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+}
