06cd573c2f58f236dd657a81ed89ec651c4d0026
hzmdwd2003 <hazemdawood2023@gmail.com>
2025-02-13 13:18:51 +0100

    Kommentare für Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,48 +1,44 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importing Link component for navigation without page reload
 
-const Navbar = ({ cartCount }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary"> {/* Bootstrap navbar with dark theme and primary background color */}
+      <div className="container"> {/* Container to keep navbar content aligned properly */}
+        <Link className="navbar-brand" to="/"> {/* Brand logo that links to the homepage */}
           MyShop
         </Link>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler" // Button to toggle navigation menu in smaller screens
           type="button"
-          onClick={() => setIsOpen(!isOpen)}
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav" // Links to the collapsible menu div below
           aria-controls="navbarNav"
-          aria-expanded={isOpen}
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span> {/* Bootstrap icon for the toggle button */}
         </button>
-        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={() => setIsOpen(false)}>
+        <div className="collapse navbar-collapse" id="navbarNav"> {/* Collapsible menu for responsive design */}
+          <ul className="navbar-nav ms-auto"> {/* Navigation list aligned to the right */}
+            <li className="nav-item"> {/* Navigation item for Home */}
+              <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/shop" onClick={() => setIsOpen(false)}>
+            <li className="nav-item"> {/* Navigation item for Shop */}
+              <Link className="nav-link" to="/shop">
                 Shop
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link position-relative" to="/cart" onClick={() => setIsOpen(false)}>
-                Cart
-                {cartCount > 0 && (
-                  <span
-                    className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                    style={{ fontSize: '0.8rem' }}
-                  >
-                    {cartCount}
-                  </span>
-                )}
+            <li className="nav-item"> {/* Navigation item for Login */}
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item"> {/* Navigation item for Register */}
+              <Link className="nav-link" to="/register">
+                Register
               </Link>
             </li>
           </ul>
@@ -52,4 +48,4 @@ const Navbar = ({ cartCount }) => {
   );
 };
 
-export default Navbar;
+export default Navbar; // Exporting Navbar component for use in other parts of the application
